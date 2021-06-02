9abdc0c09717e590191e4a3ae58ca24c3ae05ae4
Anna Beumer <anna.m.beumer@gmail.com>
2021-06-02 11:45:32 +0200

    added learn-redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import allReducer from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+  allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// import { createStore } from "redux";
+
+// // Store => globalized state
+
+// // Action => Increment
+// const increment = () => {
+//   return {
+//     type: "INCREMENT",
+//   };
+// };
+// const decrement = () => {
+//   return {
+//     type: "DECREMENT",
+//   };
+// };
+
+// // Reducer
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case "INCREMENT":
+//       return state + 1;
+//     case "DECREMENT":
+//       return state - 1;
+//   }
+// };
+
+// let store = createStore(counter);
+
+// // Display in console
+// store.subscribe(() => console.log(store.getState()));
+
+// // Dispatch
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
